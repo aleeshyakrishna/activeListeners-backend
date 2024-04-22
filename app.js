fe2b99e582e43c44b1fa6e928044f5ad5debdd4d
aleeshyakrishna <aleeshyakrishnamv@gmail.com>
2024-04-23 00:20:45 +0530

    204 error--

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(cors({
   origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-     'https://activelistenerss-frontendd-updated.vercel.app/',
-      "http://activelistenerss-frontendd.vercel.app",
+     'https://activelistenerss-frontendd-updated.vercel.app',
+      // "http://activelistenerss-frontendd.vercel.app",
       
   ], // Update with your client's origin
   methods: ["GET", "POST", "PUT", "DELETE"],
